ffc7111bcd36077dbe229ea6ea8062954a8fc876
paul <paoolkim@gmail.com>
2016-12-14 10:12:41 -0800

    screenshot command

--- index.js
+++ index.js
@@ -9,14 +9,28 @@ let env = process.env.NODE_ENV || 'development';
 
 let Horseman = require('node-horseman');
 
-let fs = require('fs');
+// move out to utils eventually
+let fs = require('fs-extra');
 let path = require('path');
 let URL = require('url');
+let _ = require('lodash');
 let dir = __dirname + '/tmp/'
 if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir)
+  fs.mkdirsSync(dir);
 }
 
+
+
+// loading middleware
+// inserts loading gif
+// edits comment to delete it when complete
+// maybe some form of progress bar in the future
+//
+// command without regex match middleware
+// spits out documentation if no match
+// user enters "/stocks "
+// bot replies with
+// "usage: /stocks <ticker>" or something.
 
 
 const Telegraf = require('telegraf');
@@ -28,7 +42,7 @@ const stocksMiddleware = new Stocks();
 // middlewares
 bot.use(Telegraf.memorySession());
 bot.hears(/stocks (.{1,5})/i, stocksMiddleware.getStocks, (ctx) => {
-	winston.log('debug','symbol: ' + ctx.match[1]);
+  winston.log('debug', 'symbol: ' + ctx.match[1]);
 });
 // bot.use(singleMiddleWareTest); // has a single function
 // bot.use(complexMiddleWare.time); // idea is to have multiple functions
@@ -46,37 +60,32 @@ bot.hears(/capture (.+)/i, (ctx) => {
 
 });
 
-bot.hears(/ss (.+)/,  (ctx) => {
+bot.hears(/ss (.+)/, (ctx) => {
 
-  let website = ctx.match[1];
-  winston.log('debug', website);
-  let parsedUrl = URL.parse(website);
+  let query = ctx.match[1];
+  let parsedUrl = URL.parse(query);
+  let website = parsedUrl.href;
+  let cleanUrl = (parsedUrl.protocol == null ? `http://${website}` : website)
+  let cleanString = (parsedUrl.protocol == 'https:' ? _.replace(website, "https://", '') : _.replace(website, "http://", ''));
+  let dirString = _.replace(cleanString, /\/+/g, "-");
+  let screenshot = `${dir}${dirString}.png`;
   let horseman = new Horseman();
 
   horseman
-    .viewport(3200,1800)
+    .viewport(3200, 1800)
     .zoom(2)
-    .open('http://www.horsemanjs.org')
-    .log('something')
-    .screenshot(__dirname + 'big.png')
+    .open(cleanUrl)
+    .screenshot(screenshot)
+    .then(() => {
+      winston.log('info', 'screenshot saved', cleanString);
+      let ss = fs.createReadStream(screenshot);
+      ctx.replyWithPhoto({ source: ss }, { caption: `screenshot of ${cleanUrl}`, disable_notification: true });
+    })
+    .catch((err) => {
+      ctx.reply(`an error occured with that URL. check logs for more info`, { reply_to_message_id: ctx.message.message_id })
+      winston.log('error', err);
+    })
     .close();
-
-  // horseman
-  //   .viewport(1600, 900)
-  //   .zoom(2)
-  //   .open(parsedUrl)
-  //   .log('i here')
-  //   .waitForNextPage()
-  //   .screenshot(__dirname + `/tmp/${parsedUrl}.png`)
-  //   .then(() => {
-  //       winston.log('debug', 'in the then function');
-  //     // var Screenshot = __dirname + `/tmp/${parsedUrl}.png`;
-
-  //     // ctx.replyWithHTML(`<strong>your regex:</strong> is ${ctx.match[1]}`)
-  //     // bot.sendPhoto(fromId, Screenshot, { caption: `btc price as of ${date}` });
-  //   })
-  //   .close();
-
 
 })
 
@@ -84,29 +93,12 @@ bot.hears(/ss (.+)/,  (ctx) => {
 
 
 
-bot.command('start', (ctx) => ctx.reply('Hey')) // type '/start'
-bot.on('sticker', (ctx) => ctx.reply('👍')) // emojis work
 
-// on any text message, increments session.counter
-// as much logic should be moved to outside middleware as possible.
-// this is just for learning / example.
-// bot.on('text', (ctx) => {
-//   winston.log('debug', ctx.session);
-//   ctx.session.counter = ctx.session.counter || 0
-//   ctx.session.counter++
-//     return ctx.reply(`Message counter:${ctx.session.counter}`)
-// })
-
-bot.hears(/capture (.+)/i, (ctx) => {
-
-  winston.log('debug', 'chat : ', ctx.chat);
-  winston.log('debug', 'regex : ', ctx.match[1]);
-
-bot.catch((err) =>{
-    winston.log('error',  err);
+bot.catch((err) => {
+  winston.log('error', err);
 })
 
 bot.startPolling()
 
 
-module.exports = bot;
+
